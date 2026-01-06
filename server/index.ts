c0465e522767938b761da33c6aede61b89c5b201
bernis-bicom <bernis@bicomsystems.com>
2026-01-06 13:18:50 +0100

    wip

--- server/index.ts
+++ server/index.ts
@@ -845,7 +845,7 @@ function renderLayout({
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&family=Spline+Sans+Mono:wght@400;600&display=swap"
+      href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&family=Sora:wght@400;500;600;700&display=swap"
       rel="stylesheet"
     />
     <link rel="stylesheet" href="/assets/styles.css" />
